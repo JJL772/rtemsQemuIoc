b2f95dcab5cb78af7b64fd037b38606d5d7c2a3e
JJL772 <jeremy.lorelli.1337@gmail.com>
2024-01-26 18:46:45 -0800

    Add iocBoot directories

---
+++ iocBoot/iocRTEMSTest/st.cmd
@@ -0,0 +1,19 @@
+#- Example RTEMS startup script
+
+#- You may have to change rtemsQemuTest to something else
+#- everywhere it appears in this file
+
+#< envPaths
+
+## Register all support components
+dbLoadDatabase("dbd/rtemsQemuTest.dbd")
+rtemsQemuTest_registerRecordDeviceDriver(pdbbase)
+
+## Load record instances
+#dbLoadTemplate("db/rtemsQemuTest.substitutions")
+#dbLoadRecords("db/rtemsQemuTest.db", "user=jeremy")
+
+iocInit
+
+## Start any sequence programs
+#seq(sncxxx, "user=jeremy")
